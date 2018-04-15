321f1af404c1638de9fed2bfdc584eed82d529e0
Marbille Juntado <marbille.juntado@descouvre.com>
2018-04-15 21:42:25 +0800

    Add user model

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -1,8 +1,10 @@
 defmodule Discuss.AuthController do
   use Discuss.Web, :controller
+  alias Discuss.User
   plug Ueberauth
 
-  def callback(conn, params) do
-    
+  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, params) do
+    user_params = %{token: auth.credentials.token, email: auth.info.email, provider: auth.provider}
+    changeset = User.changeset(%User{}, user_params)
   end
 end
