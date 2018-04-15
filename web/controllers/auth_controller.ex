d603d0f2a2811eedb43d5e43c9b46b188d3bb7de
Marbille Juntado <marbille.juntado@descouvre.com>
2018-04-15 22:58:21 +0800

    Implement auth session

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -1,10 +1,37 @@
 defmodule Discuss.AuthController do
   use Discuss.Web, :controller
-  alias Discuss.User
   plug Ueberauth
 
+  alias Discuss.User
+
   def callback(%{assigns: %{ueberauth_auth: auth}} = conn, params) do
-    user_params = %{token: auth.credentials.token, email: auth.info.email, provider: auth.provider}
+    user_params = %{token: auth.credentials.token, email: auth.info.email, provider: Atom.to_string(auth.provider)}
     changeset = User.changeset(%User{}, user_params)
+    
+    signin(conn, changeset)
+  end
+
+  defp signin(conn, changeset) do
+    case insert_or_update_user(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "Welcome back!")
+        |> put_session(:user_id, user.id)
+        |> redirect(to: topic_path(conn, :index))
+      {:error, reason} ->
+        conn
+        |> put_flash(:error, "Error signing in")
+        |> redirect(to: topic_path(conn, :index))
+    end
+  end
+
+  defp insert_or_update_user(changeset) do
+    case Repo.get_by(User, email: changeset.changes.email) do
+      nil ->
+        Repo.insert(changeset)
+      user ->
+        {:ok, user}
+    end
+     
   end
 end
