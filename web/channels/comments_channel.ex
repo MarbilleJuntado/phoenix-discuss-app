ff78470b05f1391fd48d0e1e37813e3634147548
Marbille Juntado <marbille.juntado@hotmail.ph>
2018-04-22 04:17:06 +0800

    Comments channel

--- web/channels/comments_channel.ex
+++ web/channels/comments_channel.ex
@@ -1,8 +1,10 @@
 defmodule Discuss.CommentsChannel do
   use Discuss.Web, :channel
 
-  def join() do
-    
+  def join(name, _params, socket) do
+    IO.puts("++++")
+    IO.inspect(name)
+    {:ok, %{hey: "There"}, socket}
   end
 
   def handle_in() do
