b56a5da16c76cdcb651cbd9194c7db6eb4ab966f
Marbille Juntado <marbille.juntado@hotmail.ph>
2018-04-23 00:02:02 +0800

    Sample channel message w/ a button

--- web/channels/comments_channel.ex
+++ web/channels/comments_channel.ex
@@ -2,12 +2,16 @@ defmodule Discuss.CommentsChannel do
   use Discuss.Web, :channel
 
   def join(name, _params, socket) do
-    IO.puts("++++")
-    IO.inspect(name)
     {:ok, %{hey: "There"}, socket}
   end
 
-  def handle_in() do
-    
+  # follow-up communication
+  def handle_in(name, message, socket) do
+    IO.puts("++++")
+    IO.puts(name)
+    IO.puts("++++")
+    IO.inspect(message)
+    IO.puts("++++")
+    {:reply, :ok, socket}
   end
 end
